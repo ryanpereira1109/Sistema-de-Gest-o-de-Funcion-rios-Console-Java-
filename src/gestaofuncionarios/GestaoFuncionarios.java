2804bd3b1ef3108f1b5fda508650f7cb64092f7b
ryanpereira1109 <comercialryan1109@gmail.com>
2026-01-07 15:48:43 -0300

    Sistema funcional com menu, cadastro e listage usando POO

--- src/gestaofuncionarios/GestaoFuncionarios.java
+++ src/gestaofuncionarios/GestaoFuncionarios.java
@@ -9,71 +9,24 @@ public class GestaoFuncionarios {
     public static void main(String[] args) {
         
          Scanner leia = new Scanner(System.in);
-         int escolha;        Funcionario f;
+         int escolha;       
          ArrayList<Funcionario> listaFuncionario = new ArrayList<>();
-        
-        
-       
-            
+           
         do  {  
-        mostrarMenu();
-        escolha = leia.nextInt();
-        leia.nextLine();
+           mostrarMenu();
+           escolha = leia.nextInt();
+           leia.nextLine();
 
-        switch (escolha) {
-            case 1: 
-        System.out.println("Digite seu nome: ");
-        String nome = leia.nextLine();
-        System.out.println("Digite seu cpf: ");
-        String cpf = leia.nextLine();
-        System.out.println("CLT ou PJ?: ");
-        String tipo = leia.nextLine();
- 
-        
-        switch(tipo) {
-            case "PJ":
-
-                System.out.println("Digite o valor da hora: ");
-                Double valorHora = leia.nextDouble();
-                System.out.println("Digite quantas horas trabalhadas: ");
-                Double horasTrabalhadas = leia.nextDouble();
+            switch (escolha) {
+              case 1:
+               cadastrarFuncionario(leia, listaFuncionario);
+              break;
                 
-                f = new FuncionarioPJ(nome ,cpf , valorHora, horasTrabalhadas);
-                System.out.println("Salario reajustado: " + f.calcularPagamento());
-                listaFuncionario.add(f);
+              case 2:
+                listarFuncionario(listaFuncionario);
                 break;
                 
-               
-                
-            case "CLT":
-                
-                System.out.println("Digite o salario base: ");
-                Double salarioBase = leia.nextDouble();
-                System.out.println("Digite o desconto: ");
-                Double desconto = leia.nextDouble();
-                
-                
-                f = new FuncionarioCLT(nome ,cpf, salarioBase , desconto);
-                 System.out.println("Salario reajustado: " + f.calcularPagamento());
-                 listaFuncionario.add(f);
-                
-                break;
-                
-            default:
-                System.out.println("Digite um tipo valido.");
-                break;
-                
-        }
-          break;
-                
-            case 2:
-                System.out.println("--- Lista de Funcionários ---");
-               for (Funcionario fun : listaFuncionario) {
-                System.out.println("Nome: " + fun.getNome() + " | CPF: " + fun.getCpf() + " | Tipo: " + fun.getTipo() + " | Salario: " + fun.calcularPagamento());
-            }
-                break;
-                
-            case 3:
+              case 3:
                System.out.println("--- encerrando programa ---"); 
                break;
                 
@@ -89,6 +42,47 @@ public class GestaoFuncionarios {
         System.out.println("3- Sair");
  }
         
-        
+        private static void cadastrarFuncionario(Scanner leia, ArrayList<Funcionario> lista) {
+             System.out.println("Digite seu nome: ");
+             String nome = leia.nextLine();
+
+             System.out.println("Digite seu cpf: ");
+             String cpf = leia.nextLine();
+
+             System.out.println("CLT ou PJ?: ");
+             String tipo = leia.nextLine().toUpperCase();
+
+             switch (tipo) {
+                 case "PJ":
+                 System.out.println("Digite o valor da hora: ");
+                 Double valorHora = leia.nextDouble();
+                 leia.nextLine();
+                 System.out.println("Digite quantas horas trabalhadas: ");
+                 Double horasTrabalhadas = leia.nextDouble();
+                 leia.nextLine();
+
+                 Funcionario f = new FuncionarioPJ(nome, cpf, valorHora, horasTrabalhadas);
+                 lista.add(f);
+                 break;
+
+            case "CLT":
+                 System.out.println("Digite o salario base: ");
+                 Double salarioBase = leia.nextDouble();
+                 System.out.println("Digite o desconto: ");
+                 Double desconto = leia.nextDouble();
+
+                 Funcionario f2 = new FuncionarioCLT(nome, cpf, salarioBase, desconto);
+                 lista.add(f2);
+                 break;
+                }
+     
+              }   
+     
+          public static void listarFuncionario(ArrayList<Funcionario> lista) {
+                System.out.println("--- Lista de Funcionários ---");
+                for (Funcionario fun : lista) {
+                System.out.println("Nome: " + fun.getNome() + " | CPF: " + fun.getCpf() + " | Tipo: " + fun.getTipo() + " | Salario: " + fun.calcularPagamento());
+            }
+         }
 }
     
